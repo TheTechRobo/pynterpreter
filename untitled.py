2c28ef74685ec26ef88111fd7e6b73a65964f1da
morassuttia3 <computer@Anfunfuns-MacBook-Air.local>
2020-05-21 16:59:53 -0400

    WIP

--- untitled.py
+++ untitled.py
@@ -1,9 +1,9 @@
 print("A Completely Useless Project -- pynterpreter 1.0")
 while True:
     command = input(">")
-    if "if" in command:
-        cmd2 = input("...")
-        cmd3 = input("...")
-        if cmd3 == "":
-            cmd4 = input("...")
+    #if "if" in command:
+        #cmd2 = input("...")
+        #cmd3 = input("...")
+        #if cmd3 == "":
+            #cmd4 = input("...")
     exec(command)
