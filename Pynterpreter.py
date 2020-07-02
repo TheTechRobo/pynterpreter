f0a86ef3761dd1812971570c474c9b978dff214f
ittussaromYno3 <computer@Anfunfuns-MacBook-Air.local>
2020-07-02 08:35:09 -0400

    ???fix???

--- Pynterpreter.py
+++ Pynterpreter.py
@@ -7,22 +7,22 @@ class Builtins:
     def sync():
         p("Running commands...")
         global commands
-        for item in commands:
-            exec(item)
-        commands = []
+        exec(commands)
+        commands = ''''''
 sync = Builtins.sync
 p("Setting up metadata...")
 class Metadata:
     pass
-p("Creating blank command list...")
-commands = []
+p("Creating blank command string...")
+commands = ''''''
 p("All done.                        \n")
 print("NOTE: Commands will not be run until you use command `sync()'.")
 while True:
     command = input(">>> ")
     if "sync()" in command:
         sync()
-    p("Adding command to list...")
-    commands.append(command)
+    p("Adding command to string...")
+    commands += command
+    commands += "\n"
     p("                             ")
     del command
